98e90597199a3bccbb000733c01c44a3cb19fa11
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-18 21:22:26 +0530

    Welcome to line comparison computation Program

---
+++ src/com/linecomparison/LineComparison.java
@@ -0,0 +1,11 @@
+package com.linecomparison;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to line comparison computation Program");
+
+	}
+
+}
