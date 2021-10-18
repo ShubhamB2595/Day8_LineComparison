d125c80c9690259ea210cc86eb87ce896bf747fd
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-18 22:30:31 +0530

    UC3 done, comapring two lines using the compareTo method

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -6,7 +6,7 @@ public class LineComparison {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		comparingLines();
+		lineComparing();
 		
 	}
 	
@@ -14,14 +14,16 @@ public class LineComparison {
 	private static int lengthOfLine(int x1, int y1, int x2, int y2) {
 		//calculating the length using given formula
 		int length = (int) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		System.out.println("Length of the line: " + length);	//printing length of line		
+		//System.out.println("Length of the line: " + length);	//printing length of line		
 		return length;
 	}
 
 	//method for comparing two lines
-	private static void comparingLines() {
+	private static void lineEquality() {
 		Integer line1 = lengthOfLine(2, 2, 4, 5);
+		System.out.println("Length of the line: " + line1);
 		Integer line2 = lengthOfLine(1, 2, 3, 4);
+		System.out.println("Length of the line: " + line2);
 		boolean result = line1.equals(line2);
 		if (result) {
 			System.out.println("Line1 and line2 are equal in length");
@@ -31,5 +33,22 @@ public class LineComparison {
 		}
 		
 		return;
+	}
+	
+	private static void lineComparing() {
+		Integer line1 = lengthOfLine(1, 2, 3, 4);
+		System.out.println("Length of the line: " + line1);
+		Integer line2 = lengthOfLine(8, 6, 2, 2);
+		System.out.println("Length of the line: " + line2);
+		int result = line1.compareTo(line2);
+		if (result == 1) {
+			System.out.println("Length of line1 is greater line2");
+		}
+		else if (result == 0) {
+			System.out.println("Length of line1 is equal to line2");
+		}
+		else {
+			System.out.println("Length of line1 is less line2");
+		}
 	}
 }
